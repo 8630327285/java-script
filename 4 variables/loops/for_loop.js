ec51b6bde719afba9a667f61be1cddc5bf36044f
Nitinkumar <nitinkumar18119@gmail.com>
2024-08-06 01:29:37 +0530

    code

---
+++ 4 variables/loops/for_loop.js
@@ -0,0 +1,30 @@
+
+  // let sum = 0 
+  // let n = prompt("Enter the value of n")
+  // n =Number.parseInt(n)
+  // for(let i=0; i<n; i++){
+  //   sum += (i)
+  // }
+  // console.log("sum of first " + n +" natural no is" + sum)
+  
+   
+// for loop 
+
+
+// let obj = {
+//   harry: 90,
+//   shubh: 78,
+//   nitin: 99,
+//   ritik: 99.8,
+//   ruplai: 96,
+// }
+// for(let a in obj){
+//   console.log("Marks of " + a + " are " + obj[a])
+// }
+
+
+let a = n;
+for(i = 1; i<=2; i++){
+for(j=1; j<=2; j++)
+  console.log(i+1);
+}
---
+++ 4 variables/while loop/while_loop.js
@@ -0,0 +1,27 @@
+//while lopp
+
+
+// let n = prompt("Enter the value of n")
+
+// n = Number.parseInt(n)
+
+// let i = 0;
+// while(i<n){
+//   console.log(i)
+//   i++
+// }
+
+
+// do while loop
+let n = prompt("Enter the value of n")
+n = number.parseInt(n)
+
+let i = 0;
+do{
+  console.log(i)
+  i++;
+}while(i<n)
+
+
+
+ 
