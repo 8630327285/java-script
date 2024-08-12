b7a2f25eb49c6f0cf8867473734b558571d2c89b
Nitinkumar <nitinkumar18119@gmail.com>
2024-08-13 00:45:06 +0530

    new

---
+++ 4 variables/codewithharry/javascript.js
@@ -0,0 +1,19 @@
+console.log("Nitin")
+console.table("a:1 , b:2")
+
+console.time("for loop")
+
+for(let i=0; i<=10; i++){
+  console.log(30)
+}
+console.timeEnd("for loop")
+
+console.time("while loop")
+
+let i = 0;
+while(i=0){
+  i<=30
+  console.log(30)
+  i++;
+}
+console.timeEnd("while loop")
